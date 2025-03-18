f63f93db9271028965604d1ebf09d59ce013f120
Mikhail Yudin <fagci.nsk@gmail.com>
2025-03-19 02:06:17 +0700

    FC++

--- src/apps/fc.c
+++ src/apps/fc.c
@@ -12,147 +12,125 @@
 #include "vfo1.h"
 #include <stdint.h>
 
+static const uint8_t REQUIRED_FREQUENCY_HITS = 2;
+static const uint8_t FILTER_SWITCH_INTERVAL = REQUIRED_FREQUENCY_HITS;
+
 static const char *FILTER_NAMES[] = {
     [FILTER_OFF] = "ALL",
     [FILTER_VHF] = "VHF",
     [FILTER_UHF] = "UHF",
 };
+static Filter filter = FILTER_VHF;
 
 static bool bandAutoSwitch = false;
-static uint32_t nextSwitch = 0;
 
-static FreqScanTime T = F_SC_T_0_2s;
-static uint32_t fcTimeMs;
-static uint32_t scanF = 0;
-
-static Filter filter = FILTER_OFF;
 static uint32_t bound;
 
-static const uint32_t DIFF = 300;
+static bool isScanning = true;
+static uint32_t currentFrequency = 0;
+static uint32_t lastDetectedFrequency = 0;
+static uint8_t frequencyHits = 0;
+static uint8_t filterSwitchCounter = 0;
 
-static bool scanning = false;
-
-static void startScan() {
-  T = gSettings.fcTime;
-  fcTimeMs = 200 << T;
-  BK4819_EnableFrequencyScanEx(T);
-  scanning = true;
-  Log("Scan START");
+static void enableScan() {
+  Log("FC enable");
+  BK4819_EnableFrequencyScanEx(gSettings.fcTime);
+  isScanning = true;
 }
 
-static void stopScan() {
-  BK4819_StopScan();
-  scanning = false;
-  Log("Scan STOP");
-}
-
-static void gotF(uint32_t f) {
-  stopScan();
+static void disableScan() {
+  Log("FC disable");
+  BK4819_DisableFrequencyScan();
   BK4819_RX_TurnOn();
-  RADIO_TuneTo(f);
-  RADIO_ToggleRX(true);
-  SYS_DelayMs(60);
-  gRedrawScreen = true;
-  Log("[+] f: %u", f);
-}
-
-static void switchBand(bool automatic) {
-  Log("Switch band auto=%u", automatic);
-  stopScan();
-  if (automatic) {
-    switch (filter) {
-    case FILTER_UHF:
-      filter = FILTER_VHF;
-      break;
-    case FILTER_VHF:
-    default:
-      filter = FILTER_UHF;
-      break;
-    }
-  } else {
-    filter = IncDecU(filter, 0, 3, true);
-  }
-  // scanF = 0;
-  BK4819_SelectFilterEx(filter);
-  bound = SETTINGS_GetFilterBound();
-  startScan();
+  isScanning = false;
 }
 
 void FC_init() {
+  // BK4819_Init();
+  Log("FC init");
   RADIO_LoadCurrentVFO();
-
-  gMonitorMode = false;
-  RADIO_ToggleRX(false);
-
   bound = SETTINGS_GetFilterBound();
-  startScan();
+  BK4819_SelectFilterEx(filter);
+  enableScan();
+  frequencyHits = 0;
+  filterSwitchCounter = 0;
 }
 
-void FC_deinit() {
-  stopScan();
-  BK4819_RX_TurnOn();
-}
+void FC_deinit() { disableScan(); }
 
-void FC_update() {
-  if (gIsListening) {
-    Log("Listening & check");
-    vTaskDelay(pdMS_TO_TICKS(60));
-    RADIO_CheckAndListen();
-    gRedrawScreen = true;
-    return;
+void switchFilter() {
+  if (filter == FILTER_VHF) {
+    filter = FILTER_UHF;
+  } else {
+    filter = FILTER_VHF;
   }
 
-  if (bandAutoSwitch && Now() >= nextSwitch) {
-    switchBand(true);
-    stopScan();
-    nextSwitch = Now() + (uint32_t)(fcTimeMs * 2) + 200;
-    return;
-  }
-
-  uint32_t f = 0;
-  if (scanning && !BK4819_GetFrequencyScanResult(&f)) {
-    return;
-  }
-
-  if (!scanning) {
-    startScan();
-    return;
-  }
-
-  if (!f || (f >= 8800000 && f < 10800000)) {
-    return;
-  }
+  BK4819_SelectFilterEx(filter);
+  filterSwitchCounter = 0;
   gRedrawScreen = true;
+}
 
-  /* if ((filter == FILTER_VHF && f >= bound) ||
-      (filter == FILTER_UHF && f < bound)) {
-    return;
-  } */
+void FC_update(void) {
+  if (isScanning) {
+    if (BK4819_GetFrequencyScanResult(&currentFrequency)) {
+      Log("FC got %u", currentFrequency);
+      bool freqIsOk = true;
 
-  if (filter == FILTER_VHF && f >= bound) {
-    Log("F not in VHF %u", f);
-    f /= 2;
+      gRedrawScreen = true;
+      if (bandAutoSwitch) {
+        filterSwitchCounter++;
+      }
+
+      if (currentFrequency >= 88 * MHZ && currentFrequency <= 108 * MHZ) {
+        freqIsOk = false;
+      }
+
+      if (filter == FILTER_VHF && currentFrequency >= bound) {
+        freqIsOk = false;
+      }
+
+      if (filter == FILTER_UHF && currentFrequency < bound) {
+        freqIsOk = false;
+      }
+
+      if (freqIsOk && DeltaF(currentFrequency, lastDetectedFrequency) < 300) {
+        frequencyHits++;
+      } else {
+        frequencyHits = 1;
+      }
+      Log("FC hits %u", frequencyHits);
+
+      lastDetectedFrequency = currentFrequency;
+
+      if (frequencyHits >= REQUIRED_FREQUENCY_HITS) {
+        Log("FC hit! Tuning");
+        disableScan();
+        RADIO_TuneTo(currentFrequency);
+        frequencyHits = 0;
+      } else {
+        disableScan();
+        enableScan();
+      }
+    }
+
+    if (filterSwitchCounter >= FILTER_SWITCH_INTERVAL) {
+      Log("FC switch filter");
+      switchFilter();
+    }
+    vTaskDelay(pdMS_TO_TICKS(200));
+  } else {
+    if (!gIsListening) {
+      vTaskDelay(pdMS_TO_TICKS(60));
+    }
+    Log("FC checklisten");
+    RADIO_CheckAndListen();
+    if (gIsListening) {
+      gRedrawScreen = true;
+    } else {
+      enableScan();
+    }
+    vTaskDelay(pdMS_TO_TICKS(1));
   }
-
-  if (filter == FILTER_UHF && f < bound) {
-    Log("F not in UHF %u", f);
-    f *= 2;
-  }
-
-  Loot *loot = LOOT_Get(f);
-  if (loot && (loot->blacklist || loot->whitelist)) {
-    Log("F in BL/WL %u", f);
-    return;
-  }
-
-  Log("check %u <=> %u", f, scanF);
-  if (DeltaF(f, scanF) < DIFF) {
-    gotF(f);
-    return;
-  }
-  scanF = f;
-  stopScan();
-  vTaskDelay(pdMS_TO_TICKS(1));
 }
 
 bool FC_key(KEY_Code_t key, Key_State_t state) {
@@ -187,14 +165,14 @@ bool FC_key(KEY_Code_t key, Key_State_t state) {
       LOOT_WhitelistLast();
       return true;
     case KEY_F:
-      switchBand(false);
+      switchFilter();
       return true;
     case KEY_0:
       bandAutoSwitch = !bandAutoSwitch;
       return true;
     case KEY_PTT:
       if (gLastActiveLoot) {
-        stopScan();
+        FC_deinit();
         RADIO_TuneToSave(gLastActiveLoot->f);
         gVfo1ProMode = true;
         APPS_run(APP_VFO1);
@@ -209,8 +187,8 @@ bool FC_key(KEY_Code_t key, Key_State_t state) {
 
 void FC_render() {
   PrintMediumEx(0, 16, POS_L, C_FILL, "%s %ums SQ %u %s", FILTER_NAMES[filter],
-                fcTimeMs, radio->squelch.value, bandAutoSwitch ? "[A]" : "");
-  UI_BigFrequency(40, scanF);
+                200 << gSettings.fcTime, radio->squelch.value, bandAutoSwitch ? "[A]" : "");
+  UI_BigFrequency(40, currentFrequency);
 
   if (gLastActiveLoot) {
     UI_DrawLoot(gLastActiveLoot, LCD_WIDTH, 48, POS_R);
